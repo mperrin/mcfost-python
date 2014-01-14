99315f367fad3c3a8381d6d0e20cfc01ecb7491a
Marshall Perrin <mperrin@stsci.edu>
2014-01-14 00:36:21 -0500

    progress toward chi^2 fitting in Python

---
+++ mcfost/run.py
@@ -0,0 +1,17 @@
+import astropy
+
+# Functions for actually running MCFOST models under Python control
+
+
+
+def run_sed(filename_or_Paramfile, *args, raytrace=True):
+    """ Run a MCFOST calculation of the SED"""
+    pass
+
+
+
+def run_image(filename_or_Paramfile, wavelength, *args, raytrace=True):
+    """ Run a MCFOST calculation of the image """
+    pass
+
+
